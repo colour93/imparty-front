0eb93c347220e10e811c6da1e897a721857112bf
玖叁 <colour_93@furry.top>
2024-02-20 02:03:16 +0800

    feat: remote dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const proxy = process.env.VITE_REMOTE_DEV
+  ? {
+      "/api": {
+        target: "https://api.imparty.cn:8002",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    }
+  : undefined;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 24933,
+    proxy,
   },
 });
