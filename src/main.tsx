aa4cfb16f903f692abf15e8c550f7ad3f42e8b4a
玖叁 <colour_93@furry.top>
2024-02-19 17:46:30 +0800

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <BrowserRouter>
+    <BrowserRouter basename={"/"}>
       <Router />
     </BrowserRouter>
   </HelmetProvider>
